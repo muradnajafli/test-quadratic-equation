5bc79b908a468153840390c93cfd8f9d17f8c1bb
Murad Najafli <muradnecefli05@gmail.com>
2023-06-03 16:21:46 +0400

    QuadraticEquationTaskUpdate

--- src/main/java/com/epam/rd/autotasks/QuadraticEquationTwoRootsCasesTesting.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquationTwoRootsCasesTesting.java
@@ -6,8 +6,6 @@ import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(Parameterized.class)
